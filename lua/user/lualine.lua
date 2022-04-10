2e960f84d40046335401fa7af321e28ef33f9c59
Ken Zhou <kendotzhou@gmail.com>
2022-04-10 19:42:15 -0400

    backup

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -66,8 +66,11 @@ lualine.setup({
 	options = {
 		icons_enabled = true,
 		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		component_separators = {'', ''},
+	  section_separators = {'', ''},
+    separators = {
+      component = {'', ''}
+    },
 		disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
 		always_divide_middle = true,
 	},
