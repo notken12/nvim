8ea4233262ddb2ead7d37bd8797496a6a88a3118
Ken Zhou <kendotzhou@gmail.com>
2022-04-10 20:41:27 -0400

    keep trying to fix ctrl+c closing neovim

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -29,6 +29,8 @@ vim.cmd [[
     autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
   augroup end
 
+    inoremap <C-c> <Esc>
+
 ]]
 
 
--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -2,7 +2,6 @@ vim.cmd [[
 try
   colorscheme gruvbox
 catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
   set background=dark
 endtry
 ]]
