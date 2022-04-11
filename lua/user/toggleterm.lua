fef9e76d1ba1c13326fe987c0f8832c58d635902
Ken Zhou <kendotzhou@gmail.com>
2022-04-11 16:45:42 -0400

    use bash in toggleterm

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -15,7 +15,7 @@ toggleterm.setup({
 	persist_size = true,
 	direction = "float",
 	close_on_exit = true,
-	shell = vim.o.shell,
+	shell = "bash",
 	float_opts = {
 		border = "curved",
 		winblend = 0,
