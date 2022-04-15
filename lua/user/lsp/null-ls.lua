8984b91c2bba34779d71cd1d50a3a5ba7f8633c2
Ken Zhou <kendotzhou@gmail.com>
2022-04-15 12:43:19 -0400

    improve lsp and add mogo errors

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -8,6 +8,38 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+local api = vim.api
+
+local no_really = {
+    method = null_ls.methods.DIAGNOSTICS,
+    filetypes = { "markdown", "text" },
+    generator = {
+        fn = function(params)
+            local diagnostics = {}
+            -- sources have access to a params object
+            -- containing info about the current file and editor state
+            for i, line in ipairs(params.content) do
+                local col, end_col = line:find("mogo is bad")
+                if col and end_col then
+                    -- null-ls fills in undefined positions
+                    -- and converts source diagnostics into the required format
+                    table.insert(diagnostics, {
+                        row = i,
+                        col = col,
+                        end_col = end_col,
+                        source = "mogo",
+                        message = "Don't you dare",
+                        severity = 1,
+                    })
+                end
+            end
+            return diagnostics
+        end,
+    },
+}
+
+null_ls.register(no_really)
+
 null_ls.setup({
 	debug = false,
 	sources = {
---
+++ markdown.md
@@ -0,0 +1,5 @@
+really mog
+mog
+really sog dog
+really really dog
+mogo is bad 
