8d887874e25d03c9d8d6fd9bee304055afab01b9
Ken Zhou <kendotzhou@gmail.com>
2022-05-01 22:43:47 -0400

    auto space dashboard

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -42,5 +42,40 @@ dashboard.section.header.opts.hl = "Include"
 dashboard.section.buttons.opts.hl = "Keyword"
 
 dashboard.opts.opts.noautocmd = true
--- vim.cmd([[autocmd User AlphaReady echo 'ready']])
 alpha.setup(dashboard.opts)
+
+-- ┌──────────────────────────────────────────────────────────┐
+-- │                  /                                       │
+-- │    header_padding                                        │
+-- │                  \  ┌──────────────┐ ____                │
+-- │                     │    header    │     \               │
+-- │                  /  └──────────────┘      \              │
+-- │ head_butt_padding                          \             │
+-- │                  \                          occu_        │
+-- │                  ┌────────────────────┐     height       │
+-- │                  │       button       │    /             │
+-- │                  │       button       │   /              │
+-- │                  │       button       │  /               │
+-- │                  └────────────────────┘‾‾                │
+-- │                  /                                       │
+-- │ foot_butt_padding                                        │
+-- │                  \  ┌──────────────┐                     │
+-- │                     │    footer    │                     │
+-- │                     └──────────────┘                     │
+-- │                                                          │
+-- └──────────────────────────────────────────────────────────┘
+
+local head_butt_padding = 4
+local occu_height = #dashboard.section.header.val + 2 * #dashboard.section.buttons.val + head_butt_padding
+local header_padding = math.max(0, math.ceil((vim.fn.winheight("$") - occu_height) * 0.25))
+local foot_butt_padding_ub = vim.o.lines - header_padding - occu_height - #dashboard.section.footer.val - 3
+local foot_butt_padding = math.floor((vim.fn.winheight("$") - 2 * header_padding - occu_height))
+
+dashboard.config.layout = {
+	{ type = "padding", val = header_padding },
+	dashboard.section.header,
+	{ type = "padding", val = head_butt_padding },
+	dashboard.section.buttons,
+	{ type = "padding", val = foot_butt_padding },
+	dashboard.section.footer,
+}
