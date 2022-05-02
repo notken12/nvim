3eb1b72a91e2b7dab19ad6cecd52bcffcd266c79
Ken Zhou <kendotzhou@gmail.com>
2022-05-01 23:31:31 -0400

    disable squiggles at end of file

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,10 @@
+-- Disable squiggly things at end of file
+vim.wo.fillchars = "eob: "
+
 -- For dark theme
 vim.g.vscode_style = "dark"
 -- Enable transparent background
--- vim.g.vscode_transparent = 1
+vim.g.vscode_transparent = 1
 -- Enable italic comment
 -- vim.g.vscode_italic_comment = 1
 -- Disable nvim-tree background color
