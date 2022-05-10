a149298b3a0c45351ec16d7763184685c69c0f40
Ken Zhou <kendotzhou@gmail.com>
2022-05-10 14:20:05 -0400

    empty mogo command shows random tip

--- lua/mogo.lua
+++ lua/mogo.lua
@@ -3,25 +3,41 @@ local utils = require("utils")
 local M = {}
 
 M.get_filename = function()
-  return vim.fn.stdpath("config") .. "/mogo.txt"
+	return vim.fn.stdpath("config") .. "/mogo.txt"
 end
 
 M.add_tip = function(tip)
-  local file = M.get_filename()
-  utils.append_file(file, M.trim(tip) .. "\n")
+	local file = M.get_filename()
+	utils.append_file(file, M.trim(tip) .. "\n")
 end
 
 M.trim = function(s)
-  return (s:gsub("^%s*(.-)%s*$", "%1"))
+	return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
+M.get_random_tip = function()
+	local file = M.get_filename()
+	local lines = utils.lines_from(file)
+
+	math.randomseed(os.time())
+
+	local i = math.random(#lines)
+	return lines[i]
 end
 
 M.setup = function()
-  vim.api.nvim_create_user_command("Mogo", function(args)
-    M.add_tip(args.args)
-  end, {
-    nargs = "*",
-    desc = "Add a dashboard tip",
-  })
+	vim.api.nvim_create_user_command("Mogo", function(args)
+		if args.args ~= "" then
+			-- Add tip if not empty
+			M.add_tip(args.args)
+		else
+			-- Otherwise print a random tip
+			print(M.get_random_tip())
+		end
+	end, {
+		nargs = "*",
+		desc = "Add a dashboard tip",
+	})
 end
 
 return M
