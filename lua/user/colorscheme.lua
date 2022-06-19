c932697515515f0555e31b5f572604086e1829fd
Ken Zhou <kendotzhou@gmail.com>
2022-06-19 20:54:03 +0000

    fix colorscheme load order

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -15,7 +15,7 @@ vim.opt.background = "dark"
 -- vim.opt.background = "dark"
 vim.g.gruvbox_material_background = "medium"
 vim.g.gruvbox_material_better_performance = 1
-vim.g.gruvbox_material_transparent_background = 1
+--vim.g.gruvbox_material_transparent_background = 1
 vim.cmd([[colorscheme gruvbox-material]])
 
 -- vim.cmd([[colorscheme nord]])
