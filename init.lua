31cec6a1b1b5caf324e1060ab07e026355211f12
Ken Zhou <kendotzhou@gmail.com>
2022-09-17 00:18:17 -0400

    add missing require user.colorscheme

--- init.lua
+++ init.lua
@@ -8,4 +8,5 @@ require("user.plugins")
 -- Cache the humungous packer_compiled.lua file with impatient.nvim
 -- for a solid speedup
 require("packer_compiled")
-require('user.lsp')
+require("user.lsp")
+require("user.colorscheme")
